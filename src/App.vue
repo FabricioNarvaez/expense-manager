4e639f7dc32d10f75615bc430040a8a5c738d1fe
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-01-31 15:58:04 +0100

    Budget component created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
-<script setup lang="ts">
-</script>
-
 <template>
 	<div>
 		<header>
 			<h1>Planificador de gastos</h1>
+
+			<Budget />
 		</header>
 	</div>
 </template>
+
+<script setup lang="ts">
+	import Budget from './components/Budget.vue';
+</script>
 
 <style lang="scss">
 	@import '@scss/app.scss';
---
+++ src/components/Budget.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contenedor contenedorHeader sombra">
+        <form>
+        <div class="field">
+            <label for="budget">Definir presupuesto</label>
+            <input class="newBudget" type="number" id="budget" placeholder="Añade tu presupuesto" />
+        </div>
+        <input type="submit" value="Definir presupuesto" />
+    </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
