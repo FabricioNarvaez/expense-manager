6db4bbe1ff3d9999ed970600dfebbd31bdcde1f1
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-02-12 16:57:39 +0100

    Add styles on form

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,15 +1,58 @@
 <template>
     <div class="contenedor contenedorHeader sombra">
-        <form>
-        <div class="field">
-            <label for="budget">Definir presupuesto</label>
-            <input class="newBudget" type="number" id="budget" placeholder="Añade tu presupuesto" />
-        </div>
-        <input type="submit" value="Definir presupuesto" />
-    </form>
+        <form class="budget">
+            <div class="field">
+                <label for="budget">Definir presupuesto</label>
+                <input class="newBudget" type="number" id="budget" placeholder="Añade tu presupuesto" />
+            </div>
+            <input type="submit" value="Definir presupuesto" />
+        </form>
     </div>
 </template>
 
 <script setup lang="ts">
 
 </script>
+
+<style lang="scss" scoped>
+    .budget {
+        width: 100%;
+
+        label {
+            font-size: 2.2rem;
+            text-align: center;
+            color: $blue;
+        }
+
+        input[type="number"] {
+            background-color: $light-gray;
+            border-radius: 1rem;
+            padding: 1rem;
+            border: none;
+            font-size: 2.2rem;
+            text-align: center;
+        }
+
+        input[type="submit"] {
+            background-color: $blue;
+            cursor: pointer;
+            border: none;
+            padding: 1rem;
+            text-align: center;
+            font-size: 2rem;
+            margin-top: 2rem;
+            color: $white;
+            width: 100%;
+            transition: background-color .3s ease;
+
+            &:hover {
+                background-color: $dark-blue;
+            }
+        }
+    }
+
+    .field {
+        display: grid;
+        gap: 2rem; 
+    }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@scss/app.scss";`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@scss': '/src/assets/scss',
